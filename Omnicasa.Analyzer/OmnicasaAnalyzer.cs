34d81d33d7ea014436e117417f3057a60ee07f92
hoangquach <quachhoangqb@gmail.com>
2024-02-02 13:33:09 +0700

    Add sonar analyzer

---
+++ Omnicasa.Analyzer/OmnicasaAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿namespace Omnicasa.Analyzers
+{
+    /// <summary>OmnicasaAnalyzer.</summary>
+    public class OmnicasaAnalyzer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OmnicasaAnalyzer"/> class.OmnicasaAnalyzer.
+        /// </summary>
+        public OmnicasaAnalyzer()
+        {
+        }
+    }
+}
